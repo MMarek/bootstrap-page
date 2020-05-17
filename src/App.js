a84cf94e054e731b85f36ec1f85c6c4209e0ec39
Marek Mildner <mildnermarek@gmail.com>
2020-05-17 23:58:11 +0200

    give some alerts-tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import {HashRouter, Route, Switch} from 'react-router-dom';
-import {} from "react-bootstrap";
 
 
-import Navbars from './components/Navbars/Navbars'
+import Navbars from './components/Navbars';
+import Alerts from './components/Alerts';
 
 function App() {
     return (
         <HashRouter>
             <Switch>
                 <Route exact path='/' component={Navbars}/>
+                <Route exact path='/' component={Alerts}/>
             </Switch>
         </HashRouter>
     )
---
+++ src/components/Alerts.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import Alert from 'react-bootstrap';
+//
+// function Alerts() {
+//
+//     return (
+//         [
+//             'primary',
+//             'secondary',
+//             'success',
+//             'danger',
+//             'warning',
+//             'info',
+//             'light',
+//             'dark',
+//         ].map((variant, idx) => (
+//             <Alert key={idx} variant={variant}>
+//                 This is a {variant} alert—check it out!
+//             </Alert>
+//         )));
+//
+// }
+//
+// export default Alerts;
