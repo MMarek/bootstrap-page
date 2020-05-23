e54591bc590a1dc1ce563f949574be08f12f5ee3
Marek Mildner <mildnermarek@gmail.com>
2020-05-23 17:00:50 +0200

    Updates

---
+++ precache-manifest.90158c783c116992eb05c4d3ea403892.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210199ceedb3900d21b01576552e1e9b",
+    "url": "/bootstrap-page/index.html"
+  },
+  {
+    "revision": "0b09a172fc5c02f5675d",
+    "url": "/bootstrap-page/static/css/main.624cf9df.chunk.css"
+  },
+  {
+    "revision": "40f0fa0af860afea8208",
+    "url": "/bootstrap-page/static/js/2.282a38d3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bootstrap-page/static/js/2.282a38d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b09a172fc5c02f5675d",
+    "url": "/bootstrap-page/static/js/main.d489ef57.chunk.js"
+  },
+  {
+    "revision": "ad783e88a843ca68e2d4",
+    "url": "/bootstrap-page/static/js/runtime-main.129cdf2f.js"
+  },
+  {
+    "revision": "26c726056b5472ef9b2be995244241cd",
+    "url": "/bootstrap-page/static/media/bootstrap.26c72605.png"
+  }
+]);
